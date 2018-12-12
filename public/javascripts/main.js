bebd8ce6b508a46a74b6a59723b8bfceb703b560
atoris1192 <atoris1192@MacBook12.local>
2018-12-12 16:39:58 +0900

    feature 分割

---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
+    const openMenu = document.querySelector('#open_menu')
+    const menu = document.querySelector('#menu')
+    const closeMenu = document.querySelector('#close_menu')
+
+    openMenu.addEventListener('click', () => {
+        menu.classList.add('shown')
+    })
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('shown')
+    })
+})
